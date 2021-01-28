1ada73b65cb71ac91a3fe0ecea8ad70af3bc3708
123yash8 <60549277+123yash8@users.noreply.github.com>
2021-01-28 16:03:47 +0530

    Update config.py
    made it easier for noobs

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 """
     author: feezyhendrix
-
+Yash deshmukh git 123yash8
     Configuration files
     NOTE: check Assets/proxies.txt to use your custom proxies.
  """
@@ -16,10 +16,10 @@ ASSET_DIR = os.path.join(BASE_DIR, 'Assets' )
 Config = {
     "bot_type" : 1, #change to 2 to use python requests
     "chromedriver_path": r"path/to/chromedriver",  # if windows 'chromedriver.exe'
-    "use_custom_proxy" : False, #default is False change to True to use a file containing multiple proxies of yours.
-    "use_local_ip_address" : True, #default is False chnage to True to user your computers ip directly.
-    "amount_of_account": 1, #amount of account you want to create make sure it doesnt exceed 50 for better performance
-    "amount_per_proxy": 1, #this would be amont of account used if you have a you are using multiple proxies
+    "use_custom_proxy" : False, #default is False change to True to use a file containing multiple proxies of yours. #use proxy to prevent ban of your ip
+    "use_local_ip_address" : False, #default is False chnage to True to user your computers ip directly.
+    "amount_of_account": 10, #amount of account you want to create make sure it doesnt exceed 50 for better performance #increased for noobs
+    "amount_per_proxy": 6, #this would be amont of account used if you have a you are using multiple proxies
     "proxy_file_path" : ASSET_DIR + "/proxies.txt",
     "email_domain": "gmail.com",
     "country": "it",
